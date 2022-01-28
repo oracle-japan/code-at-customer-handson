26e8df14b9a1ec22200a1554220d0eee5083cb05
Takuya Niita <takuya2230@gmail.com>
2022-01-28 17:26:12 +0900

    Browser Agent対応

---
+++ olympic_frontend_apm/src/main/resources/web/js/apm.js
@@ -0,0 +1,9 @@
+window.apmrum = (window.apmrum || {}); 
+window.apmrum.serviceName='oke-helidon-demo-frontend-UI';
+window.apmrum.webApplication='OracleJetApp';
+window.apmrum.ociDataUploadEndpoint='https://xxxxxxxxxxxxxxx.apm-agt.us-ashburn-1.oci.oraclecloud.com';
+window.apmrum.OracleAPMPublicDataKey='XXXXXXXXXXXXXXXXXXXXXXx';
+window.apmrum.traceSupportingEndpoints =  [
+  { headers: [ 'APM' ], hostPattern: '.*' },
+  { headers: [ ], hostPattern: '.*' } 
+]; 
