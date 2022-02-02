1abe66f22933ae9cd5498e9257dfc1116163d386
Takuya Niita <takuya2230@gmail.com>
2022-02-02 14:09:24 +0900

    可変部分差し戻し

--- olympic_frontend_apm/src/main/resources/web/js/apm.js
+++
@@ -1,9 +0,0 @@
-window.apmrum = (window.apmrum || {}); 
-window.apmrum.serviceName='oke-helidon-demo-frontend-UI';
-window.apmrum.webApplication='OracleJetApp';
-window.apmrum.ociDataUploadEndpoint='https://xxxxxxxxxxxxxxx.apm-agt.us-ashburn-1.oci.oraclecloud.com';
-window.apmrum.OracleAPMPublicDataKey='XXXXXXXXXXXXXXXXXXXXXXx';
-window.apmrum.traceSupportingEndpoints =  [
-  { headers: [ 'APM' ], hostPattern: '.*' },
-  { headers: [ ], hostPattern: '.*' } 
-]; 
